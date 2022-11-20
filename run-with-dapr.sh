fc05f33624cf45a7536d682f86e4e8d356d7060d
Gisela <giselatb@outlook.com>
2022-11-20 14:36:24 +0100

    modified dapr command

--- run-with-dapr.sh
+++ run-with-dapr.sh
@@ -1,1 +1,1 @@
-dapr run --log-level debug --app-id tour-of-villains-api --app-port 5111 --dapr-http-port 3501 --dapr-grpc-port 60001 -- dotnet run
+dapr run --app-id tour-of-villains-api --app-port 5111 --dapr-http-port 3501 -- dotnet run
